dc842d47e9746cc3cc196ac455b70362cfd7c5c2
Ernesto-Sevilla <estudianteernestosevilla@gmail.com>
2025-09-09 16:11:33 -0600

    Buscar coche por ID y inserta coche (Finalizado y funcional)

--- src/controllers/Coche.controllers.js
+++ src/controllers/Coche.controllers.js
@@ -13,3 +13,39 @@ export const obtenerCoches = async (req, res) => {
         });
     }
 };
+
+// Obtener un coche por su ID
+export const obtenerCoche = async (req, res) => {
+    try {
+        const [result] = await pool.query('SELECT * FROM Coche WHERE Id_Coche = ?', [req.params.Id_Coche]);
+        if (result.length <= 0) {
+            return res.status(404).json({
+                mensaje: `Error al leer los datos. ID ${req.params.Id_Coche} no encontrado.`
+            });
+        }
+        res.json(result[0]);
+    } catch (error) {
+        return res.status(500).json({
+            mensaje: 'Ha ocurrido un error al leer los datos del coche.'
+        });
+    }   
+};
+
+
+// Registrar un nuevo coche
+export const registrarCoche = async (req, res) => {
+    try {
+        const { marca, modelo, anio, placa, color, fecha_registro, estado } = req.body;
+        const [result] = await pool.query(
+        'INSERT INTO Coche (Marca, Modelo, Anio, Placa, Color, Fecha_Registro, Estado) VALUES (?, ?, ?, ?, ?, ?, ?)',
+        [marca, modelo, anio, placa, color, fecha_registro, estado]
+);
+        res.status(201).json({ id_coche: result.insertId });
+    } catch (error) {
+        return res.status(500).json({
+            mensaje: 'Ha ocurrido un error al registrar el coche.',
+            error: error
+        });
+    }
+};
+
--- src/routes/Coche.routes.js
+++ src/routes/Coche.routes.js
@@ -1,8 +1,11 @@
 import { Router } from 'express';
-import { obtenerCoches } from '../controllers/Coche.controllers.js';
+import { obtenerCoches, obtenerCoche, registrarCoche } from '../controllers/Coche.controllers.js';
 
 const router = Router();
 
 // Rutas
 router.get('/Coche', obtenerCoches);
+router.get('/Coche/:Id_Coche', obtenerCoche);
+router.post('/registrarCoche', registrarCoche);
+
 export default router;

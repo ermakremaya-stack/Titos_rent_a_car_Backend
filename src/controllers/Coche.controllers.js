121fd8dd42190606bfe3e57cb4b94a99d04b1302
Ernesto-Sevilla <estudianteernestosevilla@gmail.com>
2025-10-23 20:07:40 -0600

    Implementación de procesos almacenaos en la tabla Coches(Insertar, Actualizar, Eliminar).

--- src/controllers/Coche.controllers.js
+++ src/controllers/Coche.controllers.js
@@ -3,94 +3,96 @@ import { pool } from '../../db_connection.js';
 // Obtener todas las coches
 
 export const obtenerCoches = async (req, res) => {
-    try {
-        const [result] = await pool.query('SELECT * FROM Coche');
-        res.json(result);
-    } catch (error) {
-        return res.status(500).json({
-            mensaje: 'Ha ocurrido un error al leer los datos.',
-            error: error
-        });
-    }
+  try {
+    const [result] = await pool.query('SELECT * FROM Coche');
+    res.json(result);
+  } catch (error) {
+    return res.status(500).json({
+      mensaje: 'Ha ocurrido un error al leer los datos.',
+      error: error
+    });
+  }
 };
 
 // Obtener un coche por su ID
 export const obtenerCoche = async (req, res) => {
-    try {
-        const [result] = await pool.query('SELECT * FROM Coche WHERE Id_Coche = ?', [req.params.Id_Coche]);
-        if (result.length <= 0) {
-            return res.status(404).json({
-                mensaje: `Error al leer los datos. ID ${req.params.Id_Coche} no encontrado.`
-            });
-        }
-        res.json(result[0]);
-    } catch (error) {
-        return res.status(500).json({
-            mensaje: 'Ha ocurrido un error al leer los datos del coche.'
-        });
-    }   
+  try {
+    const [result] = await pool.query('SELECT * FROM Coche WHERE Id_Coche = ?', [req.params.Id_Coche]);
+    if (result.length <= 0) {
+      return res.status(404).json({
+        mensaje: `Error al leer los datos. ID ${req.params.Id_Coche} no encontrado.`
+      });
+    }
+    res.json(result[0]);
+  } catch (error) {
+    return res.status(500).json({
+      mensaje: 'Ha ocurrido un error al leer los datos del coche.'
+    });
+  }
 };
 
 
 // Registrar un nuevo coche
 export const registrarCoche = async (req, res) => {
-    try {
-        const { marca, modelo, anio, placa, color, fecha_registro, estado } = req.body;
-        const [result] = await pool.query(
-        'INSERT INTO Coche (Marca, Modelo, Anio, Placa, Color, Fecha_Registro, Estado) VALUES (?, ?, ?, ?, ?, ?, ?)',
-        [marca, modelo, anio, placa, color, fecha_registro, estado]
-);
-        res.status(201).json({ id_coche: result.insertId });
-    } catch (error) {
-        return res.status(500).json({
-            mensaje: 'Ha ocurrido un error al registrar el coche.',
-            error: error
-        });
-    }
+  try {
+    const { marca, modelo, anio, placa, color, fecha_registro, estado } = req.body;
+    await pool.query(
+      'CALL InsertarCoche (?, ?, ?, ?, ?, ?, ?); ',
+      [marca, modelo, anio, placa, color, fecha_registro, estado],
+    );
+    res.status(201).json({ mensaje: 'Coche registrado exitosamente.' });
+  } catch (error) {
+    return res.status(500).json({
+      mensaje: 'Ha ocurrido un error al registrar el coche.',
+      error: error
+    });
+  }
 };
 
 
 // Eliminar un coche por su ID
 export const eliminarCoche = async (req, res) => {
-    try {
-        const [result] = await pool.query('DELETE FROM Coche WHERE Id_Coche = ?', [req.params.Id_Coche]);
-        if (result.affectedRows <= 0) {
-            return res.status(404).json({
-                mensaje: `Error al eliminar. ID ${req.params.Id_Coche} no encontrado.`
-            });
-        }
-        //Respueesta exitosa
-        res.sendStatus(204).send();
-    } catch (error) {
-        return res.status(500).json({
-            mensaje: 'Ha ocurrido un error al eliminar el coche.',
-            error: error
-        });
+  try {
+    const [result] = await pool.query('CALL EliminarCoche(?)', [req.params.Id_Coche]);
+
+    if (result.affectedRows <= 0) {
+      return res.status(404).json({
+        mensaje: `Error al eliminar. ID ${req.params.Id_Coche} no encontrado.`
+      });
     }
+    //Respuesta exitosa
+    res.status(200).json({ mensaje: `Coche con ID ${req.params.Id_Coche} eliminado exitosamente.` });
+    
+  } catch (error) {
+    return res.status(500).json({
+      mensaje: 'Ha ocurrido un error al eliminar el coche.',
+      error: error
+    });
+  }
 };
 
 // Actualizar un coche por su ID
 export const actualizarCoche = async (req, res) => {
-    try {
-        const Id_Coche = req.params.Id_Coche;
-        const { marca, modelo, anio, placa, color, fecha_registro, estado } = req.body;
+  try {
+    const Id_Coche = req.params.Id_Coche;
+    const { marca, modelo, anio, placa, color, fecha_registro, estado } = req.body;
 
-        const [result] = await pool.query(
-            'UPDATE Coche SET Marca = ?, Modelo = ?, Anio = ?, Placa = ?, Color = ?, fecha_registro = ?, Estado = ? WHERE Id_Coche = ?',
-            [marca, modelo, anio, placa, color, fecha_registro, estado, Id_Coche]
-        );
-        if (result.affectedRows === 0) {
-            return res.status(404).json({
-                mensaje: `Error al actualizar el empleado. El ID ${Id_Coche} no fue encontrado.`
-            });
-        }
-        res.status(200).json({
-            mensaje: `Coche con ID ${Id_Coche} actualizado exitosamente.`
-        });
-    } catch (error) {
-        return res.status(500).json({
-            mensaje: "Ha ocurrido un error al actualizar el coche.",
-            error: error
-        });
+    const [result] = await pool.query(
+      'CALL ActualizarCoche (?, ?, ?, ?, ?, ?, ?, ?);',
+      [Id_Coche, marca, modelo, anio, placa, color, fecha_registro, estado]
+    );
+    if (result.affectedRows === 0) {
+      return res.status(404).json({
+        mensaje: `Error al actualizar el coche. El ID ${Id_Coche} no fue encontrado.`
+      });
     }
+    res.status(200).json({
+      mensaje: `Coche con ID ${Id_Coche} actualizado exitosamente.`
+    });
+  } catch (error) {
+    return res.status(500).json({
+      mensaje: "Ha ocurrido un error al actualizar el coche.",
+      error: error
+    });
+  }
 };

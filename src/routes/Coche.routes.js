c5b7c622eef86ca9cb98a03036498b35907a1bd8
Ernesto-Sevilla <estudianteernestosevilla@gmail.com>
2025-11-06 12:08:23 -0600

    Correción de actualizar coches. Ahora funcional.

--- src/routes/Coche.routes.js
+++ src/routes/Coche.routes.js
@@ -8,6 +8,6 @@ router.get('/Coches', obtenerCoches);
 router.get('/Coche/:Id_Coche', obtenerCoche);
 router.post('/registrarcoche', registrarCoche);
 router.delete('/eliminarcoche/:Id_Coche', eliminarCoche);
-router.patch('/actualizarcoche/:Id_Coche', actualizarCoche);
+router.put('/actualizarcoche/:Id_Coche', actualizarCoche);
 
 export default router;
